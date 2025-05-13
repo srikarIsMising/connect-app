28814c501b8f33a6f69f3f32c728c1061b72773d
rallabhandissrikar <srikarrallabhandi.16@outlook.com>
2025-05-13 11:57:03 +0530

    modular app creation

--- core/settings/base.py
+++ core/settings/base.py
@@ -33,6 +33,8 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'apps.home',
+    'apps.api',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
